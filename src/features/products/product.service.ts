b28519cc682bf1e42907e57796c5c2062bd6e5bd
JamesEd <edmondhomima@gmail.com>
2025-09-16 23:43:42 +0000

    Amelioration dans le product service

--- src/features/products/product.service.ts
+++ src/features/products/product.service.ts
@@ -1,4 +1,6 @@
+import { pool } from "../../db/db";
 import { Product } from "./product.models";
+
 
 const products: Product[] = [
     { id: 1, name: 'ordi', price: 1200},
@@ -6,8 +8,23 @@ const products: Product[] = [
     { id: 1, name: 'decodeur', price: 12003}
 ]
 
-export const getAllProducts = (): Product[] => products;
 
-export const getProductById = (id: number): Product | undefined => 
-    products.find((p) => p.id === id)
+export const getAllProducts = async (): Promise<Product[]> => {
+    const client = await pool.connect();
+    try {
+        const res = await client.query('SELECT * FROM products');
+        return res.rows;
+    } finally {
+        client.release();
+    }
+};
 
+export const getProductById = async (id: number): Promise<Product | undefined> => {
+    const client = await pool.connect();
+    try {
+        const res = await client.query('SELECT * FROM products WHERE id = $1', [id]);
+        return res.rows[0];
+    } finally {
+        client.release();
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import express from 'express'
 import productRoutes from './features/products/product.routes'
 import Userroutes from './features/User/userRoutes'
 
+
 const app = express();
 const PORT = 3000;
 
+
+
+app.use(express.json()); // Middleware pour parser le JSON
+
 app.use('/api/products', productRoutes)
-app.use('api/User', Userroutes)
+app.use('/api/User', Userroutes)
 
 
 app.listen(PORT, () => {
